804ef41e46a205d04726bf0b0f1948a11c39ff64
Đại Nguyễn Quang <dai13829vn@gmail.com>
2026-07-30 20:51:26 +0700

    Create baitapmangbai1.cpp

---
+++ baitapmangbai1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int a[5];
+
+	for (int i = 0;i <= (sizeof(a)/sizeof(a[0]));i++){
+		cin >> a[i];
+	}
+	for (int i = 0;i <= (sizeof(a)/sizeof(a[0]));i++){
+		cout << a[i] << " ";
+	}
+
+	return 0;
+}
